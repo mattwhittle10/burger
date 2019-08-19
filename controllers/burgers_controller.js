bd34f4e9a19ea2be2c550fa23a1c36571aefefd9
Whittle <matt.whittle10@gmail.com>
2019-08-18 21:05:08 -0600

    controller.js file created

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,14 +4,37 @@ var router = express.Router();
 
 var burger = require("../models/burgers.js");
 
-router.get("/")
-    burger.all(function(burgerData){
-        res.render("index", {burger_db})
-    })
+router.get("/", function(req, res) {
+    res.redirect("/burgers");
+});
 
+router.get("/burgers", function(req, res){
+    burger.all(function(data) {
+        var burgersObject = {
+            burger: data
+        };
+        res.render("index", burgersObject)
+    });
+});
 
-router.post("/burgers/create", function(req, res) {
-    burger.create("index", {data})
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "devoured"
+    ],[
+        req.body.burger_name, false
+    ], function() {
+        res.redirect("/burgers");
+    });
+});
 
-    
-})
+router.post("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(data) {
+        res.redirect("/burgers");
+    });
+});
+  
+module.exports = router;
